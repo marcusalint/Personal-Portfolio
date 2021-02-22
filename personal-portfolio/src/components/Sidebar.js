3c1d8adb039c2aab5295fbf514c9d151b23de110
Salar <salar.taeb@hotmail.com>
2021-02-22 00:28:27 -0800

    adding sidebar componenets

---
+++ personal-portfolio/src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Projects</li>
+        <li>Contact</li>
+        <li>Random1</li>
+        <li>Random</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Sidebar
