5e7457fdd20b2556bc0e9e76c7e319de4fc2c0d2
Salar <salar.taeb@hotmail.com>
2021-02-23 23:20:21 -0800

    importing and configuring react-router, adding home link

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Sidebar from './components/Sidebar'
+import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Link,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <Router>
       <Sidebar />
-    </div>
+      <Switch>
+        <Route path="/home" exact component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
