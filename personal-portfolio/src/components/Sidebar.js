8195e0dcce04074ede45b1c3324a633f629e0d57
Salar <salar.taeb@hotmail.com>
2021-02-23 23:20:51 -0800

    importing icons

--- personal-portfolio/src/components/Sidebar.js
+++ personal-portfolio/src/components/Sidebar.js
@@ -1,26 +1,36 @@
 import React from 'react'
 import './Sidebar.scss';
+import { FaHome,
+  FaEnvelopeSquare,
+  FaFolder,
+ } from "react-icons/fa";
+ import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   Link 
+ } from 'react-router-dom'; 
+
 
 const Sidebar = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            hello
-          </li>
-          <li>
-            yoyo
-          </li>
-          <li>
-            asdasdas
-          </li>
-          <li>
-            dasdas
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <FaHome />
+            </li>
+            <li>
+              <FaEnvelopeSquare />
+            </li>
+            <li>
+              <FaFolder />
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
   )
 }
 
