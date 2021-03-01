7b4cab81f910db5284e15c82e2d87dda8211d55c
Salar <salar.taeb@hotmail.com>
2021-02-28 20:15:14 -0800

    removing components

--- personal-portfolio/src/components/ProjectsGrid.js
+++ personal-portfolio/src/components/ProjectsGrid.js
@@ -1,28 +1,8 @@
 import React from 'react'
 import './ProjectsGrid.scss';
+import ProjectItems from './ProjectItems';
 
-const projects = [
-   {
-    id: '1',
-    name: "ReLeaf",
-    image: "https://via.placeholder.com/150",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    githubUrl: ''
-  },
-  {
-  id: '2',
-  name: "Mapper",
-  image: "https://via.placeholder.com/150",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-},
-{
-  id: '3',
-  name: "Scheduler",
-  image: "https://via.placeholder.com/150",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-},
-  
-]
+
 
 const ProjectsGrid = () => {
   return (
@@ -36,7 +16,7 @@ const ProjectsGrid = () => {
         </ul>
       </nav>
       <div>
-         <img src="img_girl.jpg" alt="Girl in a jacket"></img>
+        <ProjectItems />
       </div>
     </div>
   )
