1dd25d7e85cd1eafabf3f9c5d745f17f8eed326f
Salar <salar.taeb@hotmail.com>
2021-02-28 18:56:45 -0800

    adding projects component

---
+++ personal-portfolio/src/components/Projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Projects.scss';
+import ProjectsGrid from './ProjectsGrid';
+
+const Projects = () => {
+  return (
+    <>
+    <div id="projects">
+      <h1 id="project--page--heading">Projects</h1>
+      <ProjectsGrid />
+    </div>
+
+    </>
+  )
+}
+
+export default Projects
