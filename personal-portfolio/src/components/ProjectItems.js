8dea4a7c83ca008063f272d8397e91b4596f7203
Salar <salar.taeb@hotmail.com>
2021-02-28 23:54:12 -0800

    styling for card

--- personal-portfolio/src/components/ProjectItems.js
+++ personal-portfolio/src/components/ProjectItems.js
@@ -4,48 +4,51 @@ import {Grid} from '@material-ui/core';
 
 const ProjectItems = () => {
   const projects = [
+  {
+    id: '1',
+    name: "ReLeaf",
+    image: "https://via.placeholder.com/150",
+    description: "Releaf is a crowdfunding single page web platform for natural disaster victims. Users are able to create a campaign or search and donate to those in need.",
+    githubUrl: ''
+    },
     {
-     id: '1',
-     name: "ReLeaf",
-     image: "https://via.placeholder.com/150",
-     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-     githubUrl: ''
-   },
-   {
-   id: '2',
-   name: "Mapper",
-   image: "https://via.placeholder.com/150",
-   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
- },
- {
-   id: '3',
-   name: "Scheduler",
-   image: "https://via.placeholder.com/150",
-   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
- },
- {
-  id: '4',
-  name: "Jungle Rails",
-  image: "https://via.placeholder.com/150",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-},
-{
-  id: '5',
-  name: "Tweeter",
-  image: "https://via.placeholder.com/150",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-},
-{
-  id: '6',
-  name: "TinyURL",
-  image: "https://via.placeholder.com/150",
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-},
+    id: '2',
+    name: "Mapper",
+    image: "https://via.placeholder.com/150",
+    description: "Mapper is a web app that utilizes the Google Maps Api to allow users to create and save maps with custom markers, each marker having its own title and description. Users can favourite their own or other user generated maps as well contributing to other user’s maps."
+  },
+  {
+    id: '3',
+    name: "Scheduler",
+    image: "https://via.placeholder.com/150",
+    description: "Web application utilizing React built-in and custom hooks that allows users to add, edit and delete appointments."
+  },
+  {
+    id: '4',
+    name: "Jungle Rails",
+    image: "https://via.placeholder.com/150",
+    description: "A mini e-commerce application built with Rails 4.2."
+  },
+  {
+    id: '5',
+    name: "Tweeter",
+    image: "https://via.placeholder.com/150",
+    description: "Single page Twitter clone application"
+  },
+  {
+    id: '6',
+    name: "TinyApp",
+    image: "https://via.placeholder.com/150",
+    description: "TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLs (à la bit.ly)."
+  },
    
- ]
+]
 
  return (
-  <div className="Project--Items">
+  <Grid container
+    direction="row"
+    className="Project--Items" 
+    xs={9} >
     {projects.map((project) => (
       <div className="Project--Card">
         <img src={project.image} />
@@ -54,7 +57,7 @@ const ProjectItems = () => {
       </div>
       
     ))}
-  </div>
+  </Grid>
 );
 }
 
