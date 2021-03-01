570040144bce30e0b75cf8a5a54efb5258bed581
Salar <salar.taeb@hotmail.com>
2021-02-28 20:15:32 -0800

    mapping projects to grid

---
+++ personal-portfolio/src/components/ProjectItems.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import './ProjectItems.scss';
+import {Grid} from '@material-ui/core';
+
+const ProjectItems = () => {
+  const projects = [
+    {
+     id: '1',
+     name: "ReLeaf",
+     image: "https://via.placeholder.com/150",
+     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+     githubUrl: ''
+   },
+   {
+   id: '2',
+   name: "Mapper",
+   image: "https://via.placeholder.com/150",
+   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+ },
+ {
+   id: '3',
+   name: "Scheduler",
+   image: "https://via.placeholder.com/150",
+   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+ },
+ {
+  id: '4',
+  name: "Jungle Rails",
+  image: "https://via.placeholder.com/150",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+},
+{
+  id: '5',
+  name: "Tweeter",
+  image: "https://via.placeholder.com/150",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+},
+{
+  id: '6',
+  name: "TinyURL",
+  image: "https://via.placeholder.com/150",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+},
+   
+ ]
+
+ return (
+  <div className="Project--Items">
+    {projects.map((project) => (
+      <div className="Project--Card">
+        <img src={project.image} />
+        <p>{project.name}</p>
+        <p>{project.description}</p>
+      </div>
+      
+    ))}
+  </div>
+);
+}
+
+export default ProjectItems
