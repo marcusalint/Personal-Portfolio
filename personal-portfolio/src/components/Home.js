ee4c104f795b30aa4d0a4061dd4837942bb0e753
Salar <salar.taeb@hotmail.com>
2021-02-28 18:55:30 -0800

    finishing basic structuring

--- personal-portfolio/src/components/Home.js
+++ personal-portfolio/src/components/Home.js
@@ -3,8 +3,11 @@ import './Home.scss';
 
 const Home = () => {
   return (
-    <div>
-      <h1>hsdsjndsna</h1>
+    <div className="home">
+      <h1>Hi, I'm Salar.</h1>
+      <h1>Full Stack Developer.</h1>
+      <button id="project--button">Projects</button>
+      <button id="contact--button">Contact</button>
     </div>
   )
 }
