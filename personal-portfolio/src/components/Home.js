0a698180d57e604189ab48b01115a3136e3ff4c0
Salar <salar.taeb@hotmail.com>
2021-03-02 21:15:15 -0800

    added link for download button

--- personal-portfolio/src/components/Home.js
+++ personal-portfolio/src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Home.scss';
+import {Link} from 'react-router-dom'; 
 
 const Home = () => {
   return (
     <div className="home">
       <h1>Hi, I'm Salar.</h1>
       <h1>Full Stack Developer.</h1>
-      <button id="project--button">Projects</button>
+      <Link to="/projects"><button id="project--button">Project</button></Link>
       <button id="contact--button">Contact</button>
     </div>
   )
