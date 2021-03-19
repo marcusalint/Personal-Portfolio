cb6a3f9a8f37e21a61d03b598c57a686def5defd
Salar <salar.taeb@hotmail.com>
2021-03-18 22:25:29 -0700

    fixing margins

--- personal-portfolio/src/components/Home.js
+++ personal-portfolio/src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Home.scss';
-import MouseTrail from './MouseTrail'
+
 
 import {Link} from 'react-router-dom'; 
 
 const Home = () => {
   return (
     <div className="home">
+
       <h1>Hi, I'm Salar.</h1>
       <h1>Full Stack Developer.</h1>
       <Link to="/projects"><button id="project--button">View Projects</button></Link>
