b17c009a657fd2e24e8d559ef8dee21b9ad72f48
Salar <salar.taeb@hotmail.com>
2021-03-18 21:51:27 -0700

    removing animation

--- personal-portfolio/src/components/Home.js
+++ personal-portfolio/src/components/Home.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Home.scss';
+import MouseTrail from './MouseTrail'
 
 import {Link} from 'react-router-dom'; 
 
 const Home = () => {
   return (
     <div className="home">
-      
       <h1>Hi, I'm Salar.</h1>
       <h1>Full Stack Developer.</h1>
       <Link to="/projects"><button id="project--button">View Projects</button></Link>
       {/* <button id="contact--button">Contact</button> */}
+      
     </div>
   )
 }
