636f76ea99b0d40b463dd30413ed83ce12d8d1c7
Salar <salar.taeb@hotmail.com>
2021-04-18 22:14:08 -0700

    adding Projects  component to page for SPA structure, react scroll now working

--- personal-portfolio/src/components/Home.js
+++ personal-portfolio/src/components/Home.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Home.scss';
-
-
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+import Projects from './Projects';
 import {Link} from 'react-router-dom'; 
+import MouseTrail from './MouseTrail';
+
+
 
 const Home = () => {
-  return (
-    <div className="home">
-      
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <>
+    <MouseTrail />
+    <Sidebar isOpen={isOpen} toggle={toggle} /> 
+    <Navbar toggle={toggle}/>
+    <div name="home" className="home">
       <h1>Hi, I'm Salar.</h1>
       <h1>Full Stack Developer.</h1>
       <Link to="/projects"><button id="project--button">View Projects</button></Link>
       {/* <button id="contact--button">Contact</button> */}
-      
     </div>
+    <Projects name="projects"/>
+    </>
   )
 }
 
