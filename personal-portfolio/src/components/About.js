01cbd5ebb361cbd36d88b97a9c0f564b215f2e89
Salar <salar.taeb@hotmail.com>
2021-04-22 08:26:37 -0700

    adding minor changes

--- personal-portfolio/src/components/About.js
+++ personal-portfolio/src/components/About.js
@@ -9,11 +9,11 @@ const About = () => {
         <div className="Left--Container">
           <div className="Left--Main">
             <div className="Bio">
-              <h1 style={{textAlign: "left"}}>Who Am I?</h1>
-              <p style={{fontSize: "19px", lineHeight: "2em"}}>I'm a software developer who loves transforming cool ideas into scalable and responsive applications.</p>
+              <h1 style={{textAlign: "left"}}>About</h1>
+              <p style={{fontSize: "19px", lineHeight: "2em"}}>I'm a software developer who loves transforming cool ideas into scalable and responsive applications. </p>
             </div>
             <div className="Languages"> 
-              <h2 className="Languages--Heading">Technologies I've Worked With</h2>
+              <h2 className="Languages--Heading">Technologies</h2>
               <p>React, Express, PostgresQL, NodeJS, Ruby, Rails. JQuery, HTML, SASS, CSS, Javascript, AJAX, APIs, Material-UI </p>
             </div>
             <div className="Resume--And--Contact">
@@ -22,7 +22,7 @@ const About = () => {
                 <p id="Email">salartaeb@gmail.com</p>
               </div>
               </div>
-              <a className="View--Resume--Button" href="https://www.w3schools.com">View Resume</a>
+              <a className="View--Resume--Button" href="https://drive.google.com/file/d/1jEP4w9Vz3We85HZoJ-1h3yH7o7-7omG-/view?usp=sharing">View Resume</a>
           </div>
         </div> 
         <div className="Right--Container">
