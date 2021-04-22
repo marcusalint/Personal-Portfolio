18a7c8ba0137bbce9debba2608de0e6f9ab17338
Salar <salar.taeb@hotmail.com>
2021-04-22 06:09:31 -0700

    adding project photos for modals

--- personal-portfolio/src/components/ProjectItems.js
+++ personal-portfolio/src/components/ProjectItems.js
@@ -11,6 +11,9 @@ import {useMediaQuery} from './hooks/useMediaQuery';
     id: '1',
     name: "ReLeaf",
     image: "https://i.imgur.com/l00Hbky.png",
+    carouselImageOne: "https://github.com/marcusalint/ReLeaf/raw/master/docs/releaf_campaign.gif?raw=true",
+    carouselImageTwo: "https://github.com/marcusalint/ReLeaf/raw/master/docs/releaf_search.gif?raw=true",
+    carouselImageThree: "https://github.com/marcusalint/ReLeaf/raw/master/docs/Screen%20Shot%202021-02-02%20at%203.14.56%20PM.png?raw=true",
     description: "Releaf is a crowdfunding single page web platform for natural disaster victims. Users are able to create a campaign or search and donate to those in need.",
     githubUrl: 'https://github.com/marcusalint/ReLeaf',
     technologies: "React, PostgresQL, Express, NodeJS, Material-UI, Stripe, HTMl, SASS"
@@ -19,6 +22,8 @@ import {useMediaQuery} from './hooks/useMediaQuery';
     id: '2',
     name: "Mapper",
     image: "https://i.imgur.com/gpZt2Hf.png",
+    carouselImageOne: "https://github.com/ngampit/map-midterm-project/raw/master/Docs/Create-Delete-Marker.gif?raw=true",
+    carouselImageTwo: "https://github.com/ngampit/map-midterm-project/raw/master/Docs/Favourite_Delete%20Map.gif?raw=true",
     description: "Mapper is a web app that utilizes the Google Maps Api to allow users to create and save maps with custom markers, each marker having its own title and description. Users can favourite their own or other user generated maps as well contributing to other user’s maps.",
     githubUrl: 'https://github.com/marcusalint/Mapper',
     technologies: "Express, NodeJS, PostgresQL, Jquery, Bootstrap and SASS"
@@ -27,6 +32,8 @@ import {useMediaQuery} from './hooks/useMediaQuery';
     id: '3',
     name: "Scheduler",
     image: "https://raw.githubusercontent.com/marcusalint/scheduler/master/Images/Regular%20View%20of%20Appointments.png",
+    carouselImageOne: "https://github.com/marcusalint/scheduler/raw/master/Images/Form%20Validation%20and%20Interview%20Cancellation.png?raw=true",
+    carouselImageTwo: "https://github.com/marcusalint/scheduler/raw/master/Images/Book%20Interview.png?raw=true",
     description: "Web application utilizing React built-in and custom hooks that allows users to add, edit and delete appointments.",
     githubUrl: 'https://github.com/marcusalint/scheduler',
     technologies: "React, Express, Node.js, PostgreSQL, React, JSX, HTML, SASS and Javascript,"
@@ -35,6 +42,8 @@ import {useMediaQuery} from './hooks/useMediaQuery';
     id: '4',
     name: "Jungle Rails",
     image: "https://raw.githubusercontent.com/marcusalint/jungle-rails/master/docs/front-page.png",
+    carouselImageOne: "https://github.com/marcusalint/jungle-rails/raw/master/docs/cart-page.png?raw=true",
+    carouselImageTwo: "https://github.com/marcusalint/jungle-rails/raw/master/docs/new-product.png?raw=true",
     description: "A mini e-commerce application built with Rails 4.2.",
     githubUrl: 'https://github.com/marcusalint/jungle-rails',
     technologies: "Rails, Ruby, RSpec, PolterGeist, Bootstrap"
@@ -42,7 +51,8 @@ import {useMediaQuery} from './hooks/useMediaQuery';
   {
     id: '5',
     name: "Tweeter",
-    image: "https://raw.githubusercontent.com/marcusalint/scheduler/master/Images/Form%20Validation%20and%20Interview%20Cancellation.png",
+    image: "https://github.com/marcusalint/tweeter/raw/master/public/images/Header.png?raw=true",
+    carouselImageOne: "https://github.com/marcusalint/tweeter/raw/master/public/images/SubmitTweet.png?raw=true",
     description: "Single page Twitter clone application",
     githubUrl: 'https://github.com/marcusalint/tweeter',
     technologies: "HTML, CSS, Javascript, jQuery, AJAX"
@@ -50,7 +60,9 @@ import {useMediaQuery} from './hooks/useMediaQuery';
   {
     id: '6',
     name: "TinyApp",
-    image: "https://raw.githubusercontent.com/marcusalint/scheduler/master/Images/Book%20Interview.png",
+    image: "https://github.com/marcusalint/tinyapp/raw/main/docs/Create-New-TinyURL.png?raw=true",
+    carouselImageOne: "https://github.com/marcusalint/tinyapp/raw/main/docs/Urls-Page.png?raw=true",
+    carouselImageTwo: "https://github.com/marcusalint/tinyapp/raw/main/docs/Login-Page.png?raw=true",
     description: "TinyApp is a full stack web application built with Node and Express that allows users to shorten long URLs (à la bit.ly).",
     githubUrl: 'https://github.com/marcusalint/tinyapp',
     technologies: "Express, Node.js, EJS, Javascript"
@@ -77,6 +89,7 @@ const ProjectItems = () => {
         backgroundColor: 'rgba(66, 66, 66, 0.95)',
         backdropFilter: 'blur(4px)',
         transition: "0.4s ease-in",
+        zIndex: "999"
       },
       content: {
         borderColor: '#212121',
@@ -92,6 +105,7 @@ const ProjectItems = () => {
         top: smMedia ? '150px' : '120px',
         bottom: smMedia ? '200px' : 'auto',
         transition: "0.4s ease-in",
+        
         // maxHeight: !smMedia ? "800px" : "450px"
         }
     })
@@ -138,13 +152,16 @@ return (
     <Carousel showThumbs={false} showArrows={true} className="Carousel">
         <div>
         <img className="Modal--Main--Image" src={selectedProject && selectedProject.image} />
-        </div>
-        <div>
-          {/* <img src="https://i.picsum.photos/id/370/200/300.jpg?hmac=7gPWuhI1_LDcGkEssyW-1sPKu9NVl1KUoOs0nH7KXno" /> */}
-        </div>
-        <div>
-          {/* <img src="https://i.picsum.photos/id/370/200/300.jpg?hmac=7gPWuhI1_LDcGkEssyW-1sPKu9NVl1KUoOs0nH7KXno" />            */}
-        </div>
+      </div>
+      <div>
+        <img className="Modal--Main--Image" src={selectedProject && selectedProject.carouselImageOne} />
+      </div>
+      <div>
+        <img className="Modal--Main--Image" src={selectedProject && selectedProject.carouselImageTwo} />
+      </div>
+      <div>
+        <img className="Modal--Main--Image" src={selectedProject && selectedProject.carouselImageThree} />
+      </div>
     </Carousel>   
       <div id="Modal--Text">
       <h2>{selectedProject && selectedProject.name}</h2>
