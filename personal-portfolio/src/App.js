a98244c5a1f3ae1e7a63468920a3cd68a8723407
Salar <salar.taeb@hotmail.com>
2021-04-22 11:39:24 -0700

    changing how components are nested and avoid format errors

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import About from './components/About';
+import Projects from './components/Projects';
+import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from 'react-router-dom';
+
+
 
 function App() {
 
@@ -17,9 +22,13 @@ function App() {
         <Route  path="/" exact component={Home} />
         <Route  path="/contact" exact component={Contact} />
         <Route  path="/about" exact component={About} />
+        <Route  path="/projects" exact component={Projects} />
       </Switch>
     </Router>
+    
   );
 }
+
+  
 
 export default App;
