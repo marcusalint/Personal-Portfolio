6c0acb7c4ac3f0dba4a620a54a95fbddd8c28225
Salar <salar.taeb@hotmail.com>
2021-04-23 15:54:27 -0700

    adding social media links to footer

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Footer.scss'
 
+
 const Footer = () => {
   return (
     <div className="Footer"> 
-      
+      <a className="Social--Media--Link" href="https://www.linkedin.com/in/salartaeb/">LinkedIn</a>
+      <a className="Social--Media--Link" href="https://github.com/marcusalint">Github</a>
     </div>
   )
 }
